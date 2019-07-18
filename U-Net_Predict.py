3ea08c74249ea4b836b29a7a4e84b0b90263408b
Monologuethl <2481871325@qq.com>
2019-07-18 13:55:03 +0800

    update

---
+++ U-Net_Predict.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:CaoZhihui
+
+from __future__ import print_function
+
+import configparser as ConfigParser
+import warnings  # 不显示乱七八糟的warning
+
+from PIL import Image
+from keras import backend as K
+from keras.models import model_from_json
+
+from base_functions import get_test_data, pred_to_imgs
+
+warnings.filterwarnings("ignore")
+K.set_image_dim_ordering('th')
+
+# --read configuration file and get parameters-- #
+config = ConfigParser.RawConfigParser()
+config.read('./configuration.txt')
+path_local = config.get('unet_parameters', 'path_local')
+model_path = path_local + config.get('unet_parameters', 'unet_model_dir')
+test_images_dir = path_local + config.get('unet_parameters', 'test_images_dir')
+test_labels_dir = path_local + config.get('unet_parameters', 'test_labels_dir')
+
+print(path_local)
+print(model_path)
+print(test_images_dir)
+print(test_labels_dir)
+img_h = int(config.get('unet_parameters', 'img_h'))
+img_w = int(config.get('unet_parameters', 'img_w'))
+C = int(config.get('unet_parameters', 'C'))
+
+print('-' * 30)
+print('Loading model and weights...')
+print('-' * 30)
+model = model_from_json(open(model_path + 'unet.json').read())
+model.load_weights(model_path + 'unet_weights.h5')
+print('Loading test data...')
+print('-' * 30)
+test_x, test_y = get_test_data(test_images_dir, test_labels_dir, img_h, img_w)
+print('Predicting...')
+predictions = model.predict(test_x)
+
+pred_images = pred_to_imgs(predictions, img_h, img_w, C=C)
+
+print('-' * 30 + '\n' + 'Saving predicted images...' + '\n' + '-' * 30)
+for i in range(pred_images.shape[0]):
+    img = Image.fromarray(pred_images[i] * 255)
+    img = img.convert('L')
+    img.save('./U-Net/pred_images/pred_' + str(i) + '.png')
---
+++ U-Net_Training.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:CaoZhihui
+from __future__ import print_function
+
+import configparser as ConfigParser
+import os
+import warnings
+
+import numpy as np
+from keras import backend as K
+from keras.callbacks import ModelCheckpoint
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, Reshape, Activation, Permute, concatenate
+from keras.models import Model
+from keras.optimizers import Adam
+
+from base_functions import get_train_data
+
+warnings.filterwarnings("ignore")
+K.set_image_dim_ordering('th')
+
+# --read configuration file-- #
+config = ConfigParser.RawConfigParser()
+config.read('./configuration.txt')
+
+# --get parameters-- #
+path_local = config.get('unet_parameters', 'path_local')
+train_images_dir = path_local + config.get('unet_parameters', 'train_images_dir')
+train_labels_dir = path_local + config.get('unet_parameters', 'train_labels_dir')
+img_h = int(config.get('unet_parameters', 'img_h'))
+img_w = int(config.get('unet_parameters', 'img_w'))
+N_channels = int(config.get('unet_parameters', 'N_channels'))
+C = int(config.get('unet_parameters', 'C'))
+
+if C > 2:
+    gt_list = eval(config.get('unet_parameters', 'gt_gray_value_list'))
+else:
+    gt_list = None
+
+
+# --Build a net work-- #
+def get_net():
+    inputs = Input(shape=(N_channels, img_h, img_w))
+    # Block 1
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
+    pool1 = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(conv1)
+
+    # Block 2
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
+    pool2 = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(conv2)
+
+    # Block 3
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
+    pool3 = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(conv3)
+
+    # Block 4
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
+    pool4 = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(conv4)
+
+    # Block 5
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
+
+    up6 = concatenate([UpSampling2D(size=(2, 2))(conv5), conv4],  axis=1)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(up6)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
+
+    up7 = concatenate([UpSampling2D(size=(2, 2))(conv6), conv3],  axis=1)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(up7)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
+
+    up8 = concatenate([UpSampling2D(size=(2, 2))(conv7), conv2],  axis=1)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(up8)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
+
+    up9 = concatenate([UpSampling2D(size=(2, 2))(conv8), conv1],  axis=1)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(up9)
+
+    conv10 = Conv2D(C, (1, 1), activation='relu', kernel_initializer='he_normal')(conv9)
+
+    reshape = Reshape((C, img_h * img_w), input_shape=(C, img_h, img_w))(conv10)
+    reshape = Permute((2, 1))(reshape)
+
+    activation = Activation('softmax')(reshape)
+
+    model = Model(input=inputs, output=activation)
+
+    model.compile(optimizer=Adam(lr=1.0e-4), loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
+
+
+print('-' * 30)
+print('Loading and pre-processing train data...')
+print('-' * 30)
+
+train_x, train_y = get_train_data(train_images_dir, train_labels_dir, img_h, img_w, C=C, gt_list=gt_list)
+
+print('train_x size: ', np.shape(train_x))
+print('train_y size: ', np.shape(train_y))
+
+assert(train_y.shape[1] == img_h and train_y.shape[2] == img_w)
+train_y = np.reshape(train_y, (train_y.shape[0], img_h*img_w, C))
+model_path = path_local+config.get('unet_parameters', 'unet_model_dir')
+
+# --Check whether the output path of the model exists or not-- #
+if os.path.isdir(model_path):
+    pass
+else:
+    os.mkdir(model_path)
+# ------------------------------------ #
+print('-' * 30)
+print('Creating and compiling model...')
+print('-' * 30)
+model = get_net()
+model_checkpoint = ModelCheckpoint(model_path + '/unet.hdf5', monitor='loss', save_best_only=True)
+
+print('-' * 30)
+print('Fitting model...')
+print('-' * 30)
+batch_size = int(config.get('unet_parameters', 'batch_size'))
+epochs = int(config.get('unet_parameters', 'N_epochs'))
+val_rate = config.get('unet_parameters', 'validation_rate')
+hist = model.fit(x=train_x, y=train_y, batch_size=batch_size, epochs=epochs, verbose=1, shuffle=True,
+                 validation_split=float(val_rate), callbacks=[model_checkpoint], initial_epoch=0)
+with open(model_path + '/unet.txt', 'w') as f:
+    f.write(str(hist.history))
+print('-' * 30)
+print('Loading saved weights...')
+print('-' * 30)
+model.load_weights(model_path + '/unet.hdf5')
+json_string = model.to_json()  # equal to: json_string = model.get_config()
+open(model_path + '/unet.json', 'w').write(json_string)
+model.save_weights(model_path + '/unet_weights.h5')
